300c582d8a129d6f14686c5c834b21f60cd6f668
Prateek Lohani <67368888+Prateek-Lohani@users.noreply.github.com>
2023-12-01 17:38:21 +0530

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -2,29 +2,32 @@ var start=document.querySelector('#start');
 var pause=document.querySelector('#pause');
 var reset=document.querySelector('#reset');
 var h3=document.querySelector('h3');
-
 var interval;
+var count;
 
 start.addEventListener('click',function(){
-  
-    count=0;
+    if(count>0){
     interval=setInterval(function(){
         count++;
         h3.textContent=count;
-        
     },1000)
-
+    }
+    else{
+        count=0;
+        interval=setInterval(function(){
+            count++;
+            h3.textContent=count;     
+        },1000)
+    }
 })
 
-
 pause.addEventListener('click',function(){
-   clearInterval(interval);
+    clearInterval(interval);
+    console.log(count);
 })
 
 reset.addEventListener('click',function(){
-    interval=0;
-    count=0;
     h3.textContent=0;
-})
-
-
+    count=0;
+    clearInterval(interval);
+ })
